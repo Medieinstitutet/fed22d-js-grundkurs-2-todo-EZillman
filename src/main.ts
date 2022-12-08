bf3c1b5ef8df427f824496bc860b8e52f45ae500
EZillman <evelina.zillman@medieinstitutet.se>
2022-12-08 23:15:47 +0100

    ts errors

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,33 @@ import './style/style.scss';
 // I denna fil har vi lagrat vår "data"
 import toDos from './exampleArray';
 
+// Move variables to exampleArray?
+
 const list = document.querySelector('#thingsToDo');
 
-for (let i = 0; i < toDos.length; i++) {
-  const toDoName = toDos[i];
-  const toDoNode = document.createElement('li');
-  const toDoTextNode = document.createTextNode(toDoName);
-  toDoNode.appendChild(toDoTextNode);
-  list?.appendChild(toDoNode);
+const addToDoBtn = document.querySelector('#addToDoBtn');
+
+const newToDoName = document.querySelector('#newToDoField');
+
+// HANDLEDNING
+function addNewToDo() {
+  toDos?.push(newToDoName?.value);
+  console.log(list);
 }
+
+// Move function to utils when done
+function printToDo() {
+  list?.innerHTML = '';
+
+  for (let i = 0; i < toDos.length; i++) {
+    const toDoName = toDos[i];
+    const toDoNode = document.createElement('li');
+    const toDoTextNode = document.createTextNode(toDoName);
+    toDoNode.appendChild(toDoTextNode);
+    list?.appendChild(toDoNode);
+  }
+}
+
+printToDo();
+
+addToDoBtn?.addEventListener('click', addNewToDo);
