12c9ce4914a9ddd4e8475aabb596e8d83e509be5
EZillman <evelina.zillman@medieinstitutet.se>
2022-12-13 18:51:12 +0100

    changing method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { Linter } from 'eslint';
 import './style/style.scss';
 
 // All kod härifrån och ner är bara ett exempel för att komma igång
@@ -10,25 +11,14 @@ import './style/style.scss';
 
 // Move variables to exampleArray?
 
-const list = document.querySelector('#thingsToDo');
+/*const list = document.querySelector('#thingsToDo');
 
 const addToDoBtn = document.querySelector('#addToDoBtn');
 
-const newToDoName = document.querySelector('#newToDoField');
+const newToDoName = document.querySelector('#newToDoField');*/
 
 
-
-//const dateCreated = new Date().getTime();
-
-
-// Browser thinks this is a const unless it's in the main file
-let toDos = [
-  'Work on project',
-  'Decorate christmas tree',
-];
-
-
-printToDo();
+/*printToDo();
 
 function addNewToDo() {
   if (newToDoName.value.length === 0) {
@@ -66,20 +56,65 @@ function printToDo() {
 
     list.appendChild(toDoNode);
   }
-}
+} */
 
+//---------------------------------------------
+//------------------New method-----------------
+//---------------------------------------------
 
+//Variables
+const newToDoForm = document.querySelector('#newToDoForm');
+
+let toDos = [
+  'Work on project',
+  'Decorate christmas tree',
+]; 
+
+// Load todos from local storage
 function loadToDos() {
   toDos = JSON.parse(localStorage.getItem('toDos')) || [];
   
-  /*const toDo = {
-    content: e.target.elements.content
-    createdAt: new Date().getTime()
-  }*/
+  newToDo()
 
   localStorage.setItem('toDos', JSON.stringify(toDos));
 }
 
-addToDoBtn.addEventListener('click', addNewToDo); 
+// Creates new todos
+function newToDo(e) {
+  e.preventDefault();
+
+  const toDo = {
+    content: e.target.elements.toDoContent.value,
+    done: false,
+    createdAt: new Date().getTime()
+  }
+
+  toDos.push(toDo);
+
+  displayToDos();
+}
+
+// Makes the todos show
+function displayToDos() {
+  const toDoList = document.querySelector('#thingsToDo');
+
+  toDoList.innerHTML = '';
+
+  toDos.forEach(toDo => {
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('to-do-item');
+
+    const checkBtn = document.createElement('button');
+    const toDoText = document.createElement('input');
+    const deadlineLabel = document.createElement('label');
+    const deadlineDate = document.createElement('input');
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+  })
+}
+
+//addToDoBtn.addEventListener('click', addNewToDo); 
 
 window.addEventListener('load', loadToDos);
+newToDoForm.addEventListener('submit', newToDo); 
+
