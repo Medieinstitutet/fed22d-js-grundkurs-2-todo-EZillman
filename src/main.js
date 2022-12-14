eb645948c6c9e3c61ef8d22c8879b72f08a0c5b5
EZillman <evelina.zillman@medieinstitutet.se>
2022-12-14 19:38:13 +0100

    fixes in js and some style fixes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Linter } from 'eslint';
+
 import './style/style.scss';
 
 // All kod härifrån och ner är bara ett exempel för att komma igång
@@ -60,7 +60,7 @@ function printToDo() {
 
 //---------------------------------------------
 //------------------New method-----------------
-//---------------------------------------------
+//--------------------------------------------- 
 
 //Variables
 const newToDoForm = document.querySelector('#newToDoForm');
@@ -75,43 +75,78 @@ function loadToDos() {
   toDos = JSON.parse(localStorage.getItem('toDos')) || [];
   
   newToDo()
+  displayToDos()
 
   localStorage.setItem('toDos', JSON.stringify(toDos));
 }
 
+
+
+
 // Creates new todos
 function newToDo(e) {
-  e.preventDefault();
+  if (e !== undefined) {
+    e.preventDefault();
+    const toDo = {
+      content: e.target.elements.toDoContent.value,
+      done: false,
+      createdAt: new Date().getTime()
+    }
 
-  const toDo = {
-    content: e.target.elements.toDoContent.value,
-    done: false,
-    createdAt: new Date().getTime()
+    toDos.push(toDo);
+    displayToDos()
   }
-
-  toDos.push(toDo);
-
-  displayToDos();
 }
 
-// Makes the todos show
+
+// Makes the todos show up
 function displayToDos() {
   const toDoList = document.querySelector('#thingsToDo');
-
   toDoList.innerHTML = '';
 
   toDos.forEach(toDo => {
     const toDoItem = document.createElement('li');
     toDoItem.classList.add('to-do-item');
 
-    const checkBtn = document.createElement('button');
-    const toDoText = document.createElement('input');
+    const checkLabel = document.createElement('label');
+    const checkBtn = document.createElement('input');
+    const checkBtnStyle = document.createElement('span');
+    const toDoText = document.createElement('span');
     const deadlineLabel = document.createElement('label');
-    const deadlineDate = document.createElement('input');
-    const editBtn = document.createElement('button');
     const deleteBtn = document.createElement('button');
+
+    checkBtn.type = 'checkbox';
+    checkBtn.checked = toDo.done;
+    checkLabel.classList.add('check-label');
+    checkBtn.classList.add('check-btn');
+    checkBtnStyle.classList.add('check-btn-style');
+  
+
+    toDoText.classList.add('to-do-text');
+    deadlineLabel.classList.add('deadline');
+    deleteBtn.classList.add('delete-btn');
+
+    toDoText.innerHTML = `
+    <span id="toDoText" class="to-do-text">${toDo.content}
+    </span>`
+    deadlineLabel.innerHTML = `<label class="deadline">Deadline
+    <input type="date">
+    </label>`
+    deleteBtn.innerHTML = 'Delete'
+    
+    toDoItem.appendChild(checkLabel);
+    checkLabel.appendChild(checkBtn);
+    checkLabel.appendChild(checkBtnStyle);
+
+    toDoItem.appendChild(toDoText);
+    toDoItem.appendChild(deadlineLabel);
+    toDoItem.appendChild(deleteBtn);
+
+    toDoList.appendChild(toDoItem);
   })
+
 }
+
 
 //addToDoBtn.addEventListener('click', addNewToDo); 
 
